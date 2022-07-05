00d108af791ae7f35e25b977327512b5da82f6f4
Ryan Warren <ryanwarren678@gmail.com>
2022-07-05 13:25:38 -0500

    Adding public in server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ MongoClient.connect(uri)
     app.set("view engine", "ejs");
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
-    app.use(express.static("public"));
+    app.use(express.static(__dirname + "/public"));
 
     app.get("/", (req, res) => {
       traitsCollection

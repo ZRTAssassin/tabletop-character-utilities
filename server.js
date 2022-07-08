bce581d067c26046e6b6439174b61e3558c545c8
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2022-07-07 19:26:50 -0500

    Setting up ejs files to include navbars.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ MongoClient.connect(uri)
     const users = [];
 
     app.set("view engine", "ejs");
-    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(express.static(__dirname + "/public"));
 
@@ -27,32 +27,32 @@ MongoClient.connect(uri)
       res.json(users);
     });
 
-    app.post("/users", async (req, res) => {
-      try {
-        hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = { name: req.body.name, password: hashedPassword };
-        users.push(user);
-        res.status(201).send();
-      } catch (error) {
-        res.status(500).send();
-      }
-    });
-    app.post("/users/login", async (req, res) => {
-      const user = users.find((user) => (user.name = req.body.name));
-      if (user == null) {
-        return res.status(400).send("Cannot find user");
-      }
-      try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-          console.log(await bcrypt.compare(req.body.password, user.password));
-          res.send("Success");
-        } else {
-          res.send("Not Allowed");
-        }
-      } catch {
-        res.status(500).send();
-      }
-    });
+    // app.post("/users", async (req, res) => {
+    //   try {
+    //     hashedPassword = await bcrypt.hash(req.body.password, 10);
+    //     const user = { name: req.body.name, password: hashedPassword };
+    //     users.push(user);
+    //     res.status(201).send();
+    //   } catch (error) {
+    //     res.status(500).send();
+    //   }
+    // });
+    // app.post("/users/login", async (req, res) => {
+    //   const user = users.find((user) => (user.name = req.body.name));
+    //   if (user == null) {
+    //     return res.status(400).send("Cannot find user");
+    //   }
+    //   try {
+    //     if (await bcrypt.compare(req.body.password, user.password)) {
+    //       console.log(await bcrypt.compare(req.body.password, user.password));
+    //       res.send("Success");
+    //     } else {
+    //       res.send("Not Allowed");
+    //     }
+    //   } catch {
+    //     res.status(500).send();
+    //   }
+    // });
 
     app.get("/", (req, res) => {
       res.render("index.ejs", { name: "Ryan" });
@@ -63,6 +63,22 @@ MongoClient.connect(uri)
 
     app.get("/register", (req, res) => {
       res.render("register.ejs");
+    });
+
+    app.post("/register", async (req, res) => {
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+          id: Date.now().toString(),
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword
+        })
+        res.redirect('/')
+      } catch {
+        res.redirect('/register')
+      }
+      console.log(users);
     });
 
     app.get("/abilities", (req, res) => {

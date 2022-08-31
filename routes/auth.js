cfa8427b77064ac3ca82cbb27596d19bb6906983
Ryan Warren <ryanwarren678@gmail.com>
2022-08-31 12:38:19 -0500

    Fixing dependencies and redoing css

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth");
-
-router.get("/register", authController.getRegister);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
 const connectDB = require("./config/database");
-const homeRoutes = require("./routes/home");
+const mainRoutes = require("./routes/main");
 const traitRoutes = require("./routes/traits");
-const authRoutes = require("./routes/auth");
 const methodOverride = require("method-override");
 
 require("dotenv").config({ path: "./config/.env" });
+
+// Passport Config
+require('./config/passport')(passport)
 
 connectDB();
 
@@ -14,6 +22,22 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+  
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
 
 // Method override
 app.use(
@@ -31,9 +55,8 @@ app.use(
 );
 
 // Routes
-app.use("/", homeRoutes);
+app.use("/", mainRoutes);
 app.use("/traits", traitRoutes);
-app.use("/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");

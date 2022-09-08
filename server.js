e6670f06e64080b303799c1eff2e63d80db9811e
Ryan Warren <ryanwarren678@gmail.com>
2022-09-08 12:15:57 -0500

    Huge materialize refactor

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const flash = require('express-flash')
-const logger = require('morgan')
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("express-flash");
+const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
+const testRoutes = require("./routes/test");
 const traitRoutes = require("./routes/traits");
 const methodOverride = require("method-override");
 
 require("dotenv").config({ path: "./config/.env" });
 
 // Passport Config
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -22,22 +23,22 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logger('dev'))
+app.use(logger("dev"));
 // Sessions
 app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
-  )
-  
-// Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
-app.use(flash())
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 // Method override
 app.use(
@@ -56,6 +57,7 @@ app.use(
 
 // Routes
 app.use("/", mainRoutes);
+app.use("/test", testRoutes);
 app.use("/traits", traitRoutes);
 
 app.listen(process.env.PORT, () => {

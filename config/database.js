d771d459f620375c28596c6d36fed681b7ad8fa3
Ryan Warren <ryanwarren678@gmail.com>
2022-09-21 13:27:55 -0500

    Convert to materialize (#5)
    * Huge materialize refactor
    
    * Removing custom css for materialize css.
    
    * Adding footer/header partials to all files
    
    * Fixing register
    
    * fixed edit page
    
    * Fixed navbar styling
    
    * Fixed navbar
    
    * Attempting to fix css on inputs
    
    * Fixed drop down select
    
    * Making abilities into a table
    
    * Fixing more materialize options
    
    * Completely removing custom styles
    
    * Removing 2nd set of data
    
    * Fixed footer
    
    * Fixed formatting of  register,ejs
    
    * fixed register button formatting
    
    * Removed old code. Added hover color.
    
    * Fix login to be more like register
    
    * Removed unneeded frontend code
    
    * Updated login, register, header
    
    * Removed logout hiding
    
    * Fixing style to be more thematic
    
    * Remove unneeded files
    
    * removed old code reference

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.DB_STRING, {
+    const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    console.log(`MongoDB Connected: ${connection.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err);
     process.exit(1);
--- server.js
+++ server.js
@@ -1,61 +1,64 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const flash = require('express-flash')
-const logger = require('morgan')
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("express-flash");
+const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
+const testRoutes = require("./routes/test");
 const traitRoutes = require("./routes/traits");
 const methodOverride = require("method-override");
 
+//use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
 // Passport Config
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
+//Connect to Database
 connectDB();
 
+//Set up EJS for views
 app.set("view engine", "ejs");
+
+//Static folder
 app.use(express.static("public"));
+
+//Body parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logger('dev'))
+
+//logging
+app.use(logger("dev"));
+
+//use forms for put/delete
+app.use(methodOverride("_method"));
+
 // Sessions
 app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
-  )
-  
-// Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.use(flash())
-
-// Method override
-app.use(
-  methodOverride(function (request, response) {
-    if (
-      request.body &&
-      typeof request.body == "object" &&
-      "_method" in request.body
-    ) {
-      let method = request.body._method;
-      delete request.body._method;
-      return method;
-    }
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
-// Routes
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Use flash messages
+app.use(flash());
+
+
+
+// currently setup routes
 app.use("/", mainRoutes);
+app.use("/test", testRoutes);
 app.use("/traits", traitRoutes);
 
 app.listen(process.env.PORT, () => {
